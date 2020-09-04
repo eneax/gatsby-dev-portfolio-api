3f4b604bd07cd9266b9ed434e63f1406b71a0af8
eneax <19818937+eneax@users.noreply.github.com>
2020-09-04 19:33:13 +0200

    Update cloudinary plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,13 @@
-module.exports = ({ env }) => ({
-  upload: {
+if (process.env.NODE_ENV === 'production') {
+  module.exports = ({ env }) => ({
     provider: 'cloudinary',
     providerOptions: {
       cloud_name: env('CLOUDINARY_NAME'),
       api_key: env('CLOUDINARY_KEY'),
       api_secret: env('CLOUDINARY_SECRET'),
     },
-  },
-});
+  });
+} else {
+  // to use the default local provider you can return an empty configuration
+  module.exports = {};
+}
